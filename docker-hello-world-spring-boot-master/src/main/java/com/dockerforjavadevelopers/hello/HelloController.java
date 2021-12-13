1afa46d3a115227b6688d7e89c259e3a660122c4
Practice-Kiddo <85115495+Practice-Kiddo@users.noreply.github.com>
2021-12-13 11:54:45 +0530

    Add files via upload

---
+++ docker-hello-world-spring-boot-master/src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.dockerforjavadevelopers.hello;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World\n";
+    }
+
+}
---
+++ docker-hello-world-spring-boot-master/src/test/java/com/dockerforjavadevelopers/hello/DummyTest.java
@@ -0,0 +1,13 @@
+package com.dockerforjavadevelopers.hello;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+  @Test
+  public void aTest() {
+    assertEquals(true, true);
+  }
+}
